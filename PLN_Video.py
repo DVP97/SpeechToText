4a549fa47ca1c8690f2765791fdaaa748e0bab1d
DVP97 <45999557+DVP97@users.noreply.github.com>
2021-04-15 23:46:36 +0200

    Cambio de interfaz (QtDesigner)
    Nueva interfaz

--- PLN_Video.py
+++ PLN_Video.py
@@ -3,73 +3,61 @@
 import sys
 from functools import partial
 from PyQt5.QtCore import QEvent, QUrl, Qt
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QMainWindow,
+from PyQt5.QtWidgets import (QApplication, QDialog, QHBoxLayout, QMainWindow,
                              QWidget, QPushButton, QSlider,
                              QVBoxLayout, QFileDialog, QLabel)
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5.uic import loadUi
 
 #imports
 from moviepy.editor import *
 import speech_recognition as sr
+
+# inicializar UI
 class MainWindow(QMainWindow):
-    
     def __init__(self):
-        super().__init__()
+        super(MainWindow,self).__init__()
+        loadUi("SI_main.ui",self)
+
+        
         self.ruta = ""
         self.dir = ""
-        self.widget = QWidget(self)
-        self.layout = QVBoxLayout()
-        self.bottom_layout = QHBoxLayout()
+        # definir elementos de la UI y acciones/funciones asociadas
+        #self.widget = QWidget(self)
+        #self.layout = QVBoxLayout()
+        #self.bottom_layout = QHBoxLayout()
+        self.setWindowTitle("Speech to text from .mp4")
 
         self.video_widget = QVideoWidget(self)
         self.media_player = QMediaPlayer()
+        #Botones video
+        self.search_button.clicked.connect(self.openFile)
+        self.play_button.clicked.connect(self.play_clicked)
+        self.stop_button.clicked.connect(self.stop_clicked)
 
-        self.search_button = QPushButton("Buscar",self)
-        self.play_button = QPushButton("Iniciar Vídeo", self)
-        self.stop_button = QPushButton("Volver al principio", self)
-        self.title_label = QLabel("",self)
-        self.title_label.setStyleSheet('QLabel {background-color: black; color: green;}')
-        #self.title_label.setStyleSheet("background-color : gold")
-        self.title_label.setFixedWidth(220)
-        self.volume_label = QLabel("VOLUMEN:",self)
         self.play_button.setEnabled(False)
         self.stop_button.setEnabled(False)
-
-        self.seek_slider = QSlider(Qt.Horizontal)
-        self.volume_slider = QSlider(Qt.Horizontal)
+        
+        #Controles video
         self.volume_slider.setRange(0, 100)
         self.volume_slider.setValue(self.media_player.volume())
         self.seek_slider.sliderMoved.connect(self.media_player.setPosition)
         self.volume_slider.sliderMoved.connect(self.media_player.setVolume)
         self.media_player.positionChanged.connect(self.seek_slider.setValue)
         self.media_player.durationChanged.connect(partial(self.seek_slider.setRange, 0))
-
-        self.layout.addWidget(self.video_widget)
-        self.layout.addLayout(self.bottom_layout)
-        
-        self.bottom_layout.addWidget(self.search_button)
-        self.bottom_layout.addWidget(self.title_label)
-        self.bottom_layout.addWidget(self.play_button)
-        self.bottom_layout.addWidget(self.stop_button)
-        self.bottom_layout.addWidget(self.volume_label)
-        self.bottom_layout.addWidget(self.volume_slider)
-        self.layout.addWidget(self.seek_slider)
-
-        self.search_button.clicked.connect(self.openFile)
-        self.play_button.clicked.connect(self.play_clicked)
-        self.stop_button.clicked.connect(self.stop_clicked)
         self.media_player.stateChanged.connect(self.state_changed)
-
         self.video_widget.installEventFilter(self)
-        self.setWindowTitle("Reproductor de video")
+        """
+        
         self.resize(800, 600)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.bottom_layout.setContentsMargins(0, 0, 0, 0)
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
-    
+        """
     def play_clicked(self):
         if (self.media_player.state() in
             (QMediaPlayer.PausedState, QMediaPlayer.StoppedState)):
@@ -133,20 +121,27 @@ class MainWindow(QMainWindow):
                     self.dir = self.dir +"/"+ i
             #print(self.dir)
             
+            self.vidLayout.addWidget(self.video_widget)
             self.title_label.setText(' VIDEO: {}'.format(videoName))
             self.ruta = videoName
             VIDEO_PATH = fileName
-            self.media_player.setMedia(
-            QMediaContent(QUrl.fromLocalFile(VIDEO_PATH)))
+            self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(VIDEO_PATH)))
+
             self.media_player.setVideoOutput(self.video_widget)
+            self.video_widget.setGeometry(30,30,700,300)
+            
             self.play_button.setEnabled(True)
             self.stop_button.setEnabled(True)
-        
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-sys.exit(app.exec_())##
 
+#inicializar app
+app=QtWidgets.QApplication(sys.argv)
+#crear instancia clase initial
+mainwindow=MainWindow()
+
+#Stack
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.show()
+app.exec()
 
     
